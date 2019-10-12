2d78e4e7e7b772c9d0c049b7f172242b3d3090dc
jeetgpatel45 <jeetgpatel45@yahoo.com>
2019-10-12 11:53:16 -0400

    adding stuff

--- server.js
+++ server.js
@@ -15,8 +15,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+var MONGODB = "mongodb://heroku_39kx9dwz:Gaurang4185!@ds333248.mlab.com:33248/heroku_39kx9dwz"
+
 var connectMongo = process.env.connectMongo || "mongodb://localhost/webScrapper";
-mongoose.connect(connectMongo);
+mongoose.connect(MONGODB);
+
 
 app.get("/", function (req, res) {
     res.json(path.join(__dirname, "./public/index.html"));
